73a327cb1bc37e286c28cc351c9fcab0d1aaee3f
safwanunited <76682718+safwanunited@users.noreply.github.com>
2021-06-14 17:32:13 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,6 @@ public class HelloWorld{
 		public static void main(String []args){
 			String firstname=args[0];
 			String lastname=args[1];
-			System.out.println("Welcome to Java");
+			System.out.println("Welcome to Java"+firstname +""+lastname);
 		}
 }
